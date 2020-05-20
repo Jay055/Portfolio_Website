3ddcada4c909e0a7212228cb227af7e522296a65
jay055 <nwachukwuuche@gmail.com>
2020-05-20 23:38:05 +0200

    Added Srapi

--- src/components/Services.js
+++ src/components/Services.js
@@ -31,3 +31,24 @@ const Services = () => {
 }
 
 export default Services
+
+// API WITH STRAPI 
+// Use Strapi To Set Up back end 
+// Strapi.io 
+// npm create strap-app my-project --quick start 
+// Set up Admin in local host 
+// Content types builder 
+// Crate new collection type
+// Set up a name with a singular name 
+// Make each field required to 
+
+// Create New Component 
+// Create new component 
+// Go back to the Job's collection and link the component
+
+ // Add Fields 
+
+//<----- ACCESS API DATA --------> 
+// To access api data we grant public permissions 
+// http://localhost:1337/jobs (where jobs is our content type)
+// 
