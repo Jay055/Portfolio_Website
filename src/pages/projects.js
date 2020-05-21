b278f246c2eea8f31b51676b49a1688f9dde8ff5
jay055 <nwachukwuuche@gmail.com>
2020-05-21 13:37:13 +0200

    Added Projects Page

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -4,8 +4,43 @@ import { graphql } from "gatsby"
 import Projects from "../components/Projects"
 // ...GatsbyImageSharpFluid
 
-const ProjectsPage = () => {
-  return <h2>projects page</h2>
+
+// Destructure data from GraphQl query
+const ProjectsPage = ({
+  data:{allStrapiProjects:{nodes:projects}}
+}) => {
+  return <Layout>
+    <section className="projects-page">
+      <Projects projects={projects} title="all projects" /> 
+    </section>
+  </Layout> 
 }
+
+export const query = graphql`
+  {
+    allStrapiProjects {
+      nodes {
+        github
+        id
+        description
+        stack {
+          id
+          title
+        }
+        title
+        url
+        image {
+          childImageSharp {
+            fluid {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+      }
+    }
+  }
+`
+
+
 
 export default ProjectsPage
